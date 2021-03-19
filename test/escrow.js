7fb713e30edc13ca188cdba583fa0897a2d4cb52
Sergi Juanati <wint3rmute@gmail.com>
2021-03-19 11:02:07 +0100

    Added balance check with @openzeppelin/test-environment

--- test/escrow.js
+++ test/escrow.js
@@ -1,5 +1,8 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
-const Escrow = artifacts.require('Escrow.sol');
+const { expectRevert, balance, ether, BN } = require('@openzeppelin/test-helpers');
+const { accounts, contract } = require('@openzeppelin/test-environment');
+
+//const Escrow = artifacts.require('Escrow.sol');
+const Escrow = contract.fromArtifact('Escrow');
 
 const STATUS = {
     Order: 0,
@@ -11,18 +14,24 @@ const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 const ethToWei = (item) => web3.utils.toWei(item);
 
-contract('Escrow', accounts => {
+// contract('Escrow', accounts => {
+//     let escrow;
+//     const [admin, buyer1, buyer2, seller1, seller2] = accounts;
+
+describe('Escrow', function () {
+    const [admin, buyer1, seller1] = accounts;
     let escrow;
-    const [admin, buyer1, buyer2, seller1, seller2] = accounts;
 
     beforeEach(async () => {
-        escrow = await Escrow.new();
+        // escrow = await Escrow.new();
+        // await escrow.offer('apple', 2, 10, { from: seller1 });
+        escrow = await Escrow.new({ from: admin });
         await escrow.offer('apple', 2, 10, { from: seller1 });
     });
 
     /**************************** O F F E R **************************** */
 
-    it('should create an offer', async() => {
+    it('should create an offer', async () => {
         let item = await escrow.items.call('apple');
 
         assert(item.seller === seller1, 'seller does not match with item');
@@ -30,7 +39,7 @@ contract('Escrow', accounts => {
         assert(item.amount.toNumber() === 10, 'amount does not match with item');
     });
 
-    it('should update an offer', async() => {
+    it('should update an offer', async () => {
         await escrow.offer('apple', 4, 5, { from: seller1 });
         let item = await escrow.items.call('apple');
 
@@ -38,19 +47,19 @@ contract('Escrow', accounts => {
         assert(item.amount.toNumber() === 15, 'amount does not match with item');
     });
 
-    it('should not create an offer', async() => {
+    it('should not create an offer', async () => {
         await expectRevert(
-            escrow.offer('table', 0, 3, {from: seller1}),
+            escrow.offer('table', 0, 3, { from: seller1 }),
             'amount & price must be higher than 0'
         );
     });
 
     /**************************** O R D E R **************************** */
 
-    it('should create an order', async() => {
-        await escrow.order('apple', 2, {from: buyer1, value: 4});
-        const res = await escrow.getOrder('apple', {from: buyer1});
-        const {0: ordered, 1: status} = res;
+    it('should create an order', async () => {
+        await escrow.order('apple', 2, { from: buyer1, value: 4 });
+        const res = await escrow.getOrder('apple', { from: buyer1 });
+        const { 0: ordered, 1: status } = res;
 
         assert(ordered.toNumber() === 2, 'ordered amount does not match');
         assert(status.toNumber() === STATUS.Order, 'status does not match');
@@ -60,79 +69,90 @@ contract('Escrow', accounts => {
         assert(item.amount.toNumber() === 8, 'remaining amount does not match');
     });
 
-    it('should not create an order - item does not exist', async() => {
+    it('should not create an order - item does not exist', async () => {
         await expectRevert(
-            escrow.order('pineapple', 2, {from: buyer1, value: 4}),
+            escrow.order('pineapple', 2, { from: buyer1, value: 4 }),
             'Item does not exist'
         );
     });
 
-    it('should not create an order - amount not available', async() => {
+    it('should not create an order - amount not available', async () => {
         await expectRevert(
-            escrow.order('apple', 15, {from: buyer1, value: 4}),
+            escrow.order('apple', 15, { from: buyer1, value: 4 }),
             'Amount not available'
         );
     });
 
-    it('should not create an order - wrong price', async() => {
+    it('should not create an order - wrong price', async () => {
         await expectRevert(
-            escrow.order('apple', 2, {from: buyer1, value: 1}),
+            escrow.order('apple', 2, { from: buyer1, value: 1 }),
             'Price sent is not equal to product price'
         );
     });
 
     /************************* C O M P L E T E ************************* */
 
-    it('should complete an order', async() => {
-        await escrow.order('apple', 2, {from: buyer1, value: 4});
-        await escrow.complete('apple', {from: buyer1});
+    it.only('should complete an order', async () => {
 
-        const res = await escrow.getOrder('apple', {from: buyer1});
-        const {0: ordered, 1: status} = res;
+        const balanceBuyerBefore = await balance.current(buyer1);
+        const balanceSellerBefore = await balance.current(seller1);
+
+        await escrow.order('apple', 2, { from: buyer1, value: 4 });
+        await escrow.complete('apple', { from: buyer1 });
+
+        const res = await escrow.getOrder('apple', { from: buyer1 });
+        const { 0: ordered, 1: status } = res;
 
         assert(ordered.toNumber() === 2, 'ordered amount does not match');
         assert(status.toNumber() === STATUS.Complete, 'status does not match');
+
+        const balanceBuyerAfter = await balance.current(buyer1);
+        const balanceSellerAfter = await balance.current(seller1);
+
+        // Check balances
+        assert(balanceBuyerAfter.eq(balanceBuyerBefore.sub(new BN(4))), 'final buyers balance');
+        assert(balanceSellerAfter.eq(balanceSellerBefore.add(new BN(4))), 'final sellers balance');
     });
 
-    it('should not complete an order - item not ordered', async() => {
+    it('should not complete an order - item not ordered', async () => {
         await expectRevert(
-            escrow.complete('banana', {from: buyer1}),
+            escrow.complete('banana', { from: buyer1 }),
             'Item not ordered or in different status than ORDER'
         );
     });
 
-    it('should not complete an order - status is different than <ORDER>', async() => {
+    it('should not complete an order - status is different than <ORDER>', async () => {
         await expectRevert(
-            escrow.complete('apple', {from: buyer1}),
+            escrow.complete('apple', { from: buyer1 }),
             'Item not ordered or in different status than ORDER'
         );
     });
 
-    //TODO: check eth transfer from contract to seller
+
 
     /************************* C O M P L A I N ************************* */
 
-    it.only('should complete an order', async() => {
-        await escrow.order('apple', 2, {from: buyer1, value: 4});
-        await escrow.complain('apple', {from: buyer1});
+    it('should complete an order', async () => {
+        await escrow.order('apple', 2, { from: buyer1, value: 4 });
+        await escrow.complain('apple', { from: buyer1 });
 
-        const res = await escrow.getOrder('apple', {from: buyer1});
-        const {0: ordered, 1: status} = res;
+        const res = await escrow.getOrder('apple', { from: buyer1 });
+        const { 0: ordered, 1: status } = res;
 
         assert(ordered.toNumber() === 2, 'ordered amount does not match');
         assert(status.toNumber() === STATUS.Complain, 'status does not match');
     });
 
-    it.only('should not complete an order - item not ordered', async() => {
+    it('should not complete an order - item not ordered', async () => {
         await expectRevert(
-            escrow.complain('banana', {from: buyer1}),
+            escrow.complain('banana', { from: buyer1 }),
             'Item not ordered or in different status than ORDER'
         );
     });
 
-    it.only('should not complete an order - status is different than <ORDER>', async() => {
+    it('should not complete an order - status is different than <ORDER>', async () => {
         await expectRevert(
-            escrow.complain('apple', {from: buyer1}),
+            escrow.complain('apple', { from: buyer1 }),
             'Item not ordered or in different status than ORDER'
         );
     });
